c9eed230ba4cc222131881e705554efba1be6add
Mathias231 <mathias.krogh@outlook.com>
2022-09-06 10:59:07 +0000

    Display products component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Products from '../components/products'
 
 const Home: NextPage = () => {
+
   return (
     <div>
-
+      <Products />
     </div>
   )
 }
