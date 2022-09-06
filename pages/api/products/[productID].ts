b19b2dce4868637ef26ed1d57d7faadfd9702f07
Mathias231 <mathias.krogh@outlook.com>
2022-09-06 10:59:16 +0000

    Tidying

--- pages/api/products/[productID].ts
+++ pages/api/products/[productID].ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { connectDB } from '../../../lib/server/db';
 import { handler } from '../../../middleware/handler';
@@ -7,9 +6,11 @@ import { Prisma } from '@prisma/client';
 type GetData = {
   name: string;
 };
+
 type Error = {
   error: string;
 };
+
 interface Request extends NextApiRequest {
   query: {
     productID: string;
