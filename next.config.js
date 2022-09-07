c7f4c11dcfe2f08267060f07fd355368a093b99e
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-09-07 14:53:26 +0000

    added test domain for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  images: {
+    domains: ['loremflickr.com'],
+  },
+};
+module.exports = nextConfig;
