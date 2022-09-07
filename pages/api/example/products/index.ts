b0836f71d7d3f9061c66b6d847946b52b4c5bc84
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-09-07 14:54:34 +0000

    Api for pagination

--- pages/api/example/products/index.ts
+++ pages/api/example/products/index.ts
@@ -6,7 +6,14 @@ import nextConnect from 'next-connect';
 export type ErrorResponse = {
   error: string;
 };
-
+export type Response = {
+  products: Prisma.ProductsGetPayload<{ include: { Review: false } }>[];
+  pagination: {
+    page: number;
+    totalPages: number;
+    limit: number;
+  };
+};
 export const handler = nextConnect({
   onError: (
     err,
@@ -30,15 +37,10 @@ interface Request extends NextApiRequest {
 }
 
 handler.get(
-  async (
-    req: Request,
-    res: NextApiResponse<
-      | Prisma.ProductsGetPayload<{ include: { Review: false } }>[]
-      | ErrorResponse
-    >,
-  ) => {
+  async (req: Request, res: NextApiResponse<Response | ErrorResponse>) => {
     const { offset, limit } = req.query;
     // checks if offset and limit are numbers
+
     if (isNaN(Number(offset)) || isNaN(Number(limit))) {
       res.status(400).json({ error: 'Offset and limit must be numbers' });
       return;
@@ -47,12 +49,16 @@ handler.get(
     // checks if offset and limit are postive numbers
     const offsetNumber = Number(offset);
     let limitNumber = Number(limit);
-    if (offsetNumber <= 0 || limitNumber <= 0) {
-      res
-        .status(400)
-        .json({ error: 'Offset and limit must be positive and not zero' });
+    if (limitNumber <= 0) {
+      res.status(400).json({ error: 'Limit must be positive and not zero' });
       return;
     }
+
+    if (offsetNumber < 0) {
+      res.status(400).json({ error: 'Offset must be positive' });
+      return;
+    }
+
     // limit must be less than 20 to prevent overloading the server
     if (limitNumber > 20) {
       res.status(400).json({ error: 'Limit must be less than 20' });
@@ -61,7 +67,7 @@ handler.get(
     const { prisma } = await connectDB();
     // check if offset is greater than the number of products in the database
     const count = await prisma.products.count();
-    if (offsetNumber > count) {
+    if (offsetNumber > count - 1) {
       res
         .status(400)
         .json({ error: 'Offset is greater than the number of products' });
@@ -69,9 +75,10 @@ handler.get(
     }
     // if offset + limit is greater than the number of products in the database
     // then limit is set to the number of products in the database - offset
-    if (offsetNumber + limitNumber > count) {
+    let newlimitNumber = limitNumber;
+    if (offsetNumber + limitNumber > count - 1) {
       console.log(count - offsetNumber);
-      limitNumber = count - offsetNumber;
+      newlimitNumber = count - offsetNumber;
     }
 
     let products = await prisma.products.findMany({
@@ -81,7 +88,16 @@ handler.get(
         Review: false,
       },
     });
-    return res.status(200).send(products);
+    const response: Response = {
+      products,
+      pagination: {
+        page: Math.floor(offsetNumber / limitNumber) + 1,
+        totalPages: Math.ceil(count / limitNumber),
+        limit: limitNumber,
+      },
+    };
+    // if there are no products in the database
+    return res.status(200).send(response);
   },
 );
 
