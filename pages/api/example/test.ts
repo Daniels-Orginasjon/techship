69082b9cfe75d66855f1a7579bbd1981eb17b090
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-09-14 06:54:34 +0000

    added uniqueId to the schema

---
+++ pages/api/example/test.ts
@@ -0,0 +1,23 @@
+import { prisma, Prisma } from '@prisma/client';
+import { connectDB, createUser, findUser } from '../../../lib/server/db';
+import { NextApiRequest, NextApiResponse } from 'next';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import bcrypt from 'bcrypt';
+import { errorHandler } from '../../../middleware/handler';
+import nc from 'next-connect';
+import { v4 as uuidv4 } from 'uuid';
+const handler = nc(errorHandler);
+
+export interface CreateUserPost extends NextApiRequest {
+  body: {
+    email: string[] | string | undefined;
+    username: string[] | string | undefined;
+    createPassword: string[] | string | undefined;
+    confirmPassword: string[] | string | undefined;
+  };
+}
+
+handler.post(async (req: CreateUserPost, res: NextApiResponse) => {});
+
+export default handler;
