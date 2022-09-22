08f6ad119d6536b147066da8ad2e1d0216f8151c
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-09-22 11:45:13 +0200

    Added issue comment

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,7 @@
 import nextConnect from 'next-connect';
 import passport from '../lib/server/passport';
 import session from '../lib/server/session';
-
+// ISSUE: Should check if env variable IRON_SESSION_SECRET is set and throw error if not.
 const auth = nextConnect()
   .use(
     session({
