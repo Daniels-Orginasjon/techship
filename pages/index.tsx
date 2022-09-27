f35fd745ecdcf5b748248dbdd5d7e1242dee7647
Mathias231 <mathias.krogh@outlook.com>
2022-09-27 11:05:23 +0000

    Starting with ads (commented out)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next';
+import Script from 'next/script';
 import SlideShow from '../components/slideshows/slideshow';
 
 const Home: NextPage = () => {
   return (
     <div className="min-h-full">
       <SlideShow />
+
+      {/* <Script
+        async
+        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      ></Script>
+      <ins
+        className="adsbygoogle"
+        data-ad-client="ca-pub-987************676"
+        data-ad-slot="776****95"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
     </div>
   );
 };
